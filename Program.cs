f85c5b8efd905937dff24b50280b3528c6b073cc
Nguyen Thanh Lan <tkthoycungkho@gmail.com>
2025-07-11 23:27:27 +0700

    Hoan thanh C# co ban, them .gitignore

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Nhập vào 3 số a,b,c: ");
+Console.Write("a = ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("b = ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("c = ");
+double c = double.Parse(Console.ReadLine());
+
+double delta = (double) Math.Pow(b, 2);
+
+if  (delta < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Phương trình vô nghiệm.");
+    Console.ResetColor();
+}
+
+else if (delta ==0)
+{
+    double x = (double)-b / (2 * a);
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"Phương trình có nghiệm kép là : x = {x} ");
+    Console.ResetColor();
+}
+else
+{
+    double x1 = (double)(-b + Math.Sqrt(delta)) / (2 * a); 
+    double x2 = (double)(-b - Math.Sqrt(delta)) / (2 * a);
+    Console.ForegroundColor= ConsoleColor.Green;
+    Console.WriteLine($"Phương trình có 2 nghiệm phân biệt x1 = {x1}, x2 = {x2}");
+    Console.ResetColor();
+}
