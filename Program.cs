602b54111a91e554f984924b08976db712489a94
Nguyen Thanh Lan <tkthoycungkho@gmail.com>
2025-07-11 23:35:36 +0700

    Bai 1.1: Tao chuong trinh Hello Git

--- Program.cs
+++ Program.cs
@@ -1,36 +1,8 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿using System.Text;
 
-Console.WriteLine("Nhập vào 3 số a,b,c: ");
-Console.Write("a = ");
-double a = double.Parse(Console.ReadLine());
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Console.Write("b = ");
-double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Hello, Git!");
 
-Console.Write("c = ");
-double c = double.Parse(Console.ReadLine());
-
-double delta = (double) Math.Pow(b, 2);
-
-if  (delta < 0)
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Phương trình vô nghiệm.");
-    Console.ResetColor();
-}
-
-else if (delta ==0)
-{
-    double x = (double)-b / (2 * a);
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine($"Phương trình có nghiệm kép là : x = {x} ");
-    Console.ResetColor();
-}
-else
-{
-    double x1 = (double)(-b + Math.Sqrt(delta)) / (2 * a); 
-    double x2 = (double)(-b - Math.Sqrt(delta)) / (2 * a);
-    Console.ForegroundColor= ConsoleColor.Green;
-    Console.WriteLine($"Phương trình có 2 nghiệm phân biệt x1 = {x1}, x2 = {x2}");
-    Console.ResetColor();
-}
+Console.WriteLine("\n Nhấn một phím bất kỳ để thoát...");
+Console.ReadKey();
